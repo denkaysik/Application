556b841b664feab28e398b8f99cc1ddbb58d9da7
denkaysik <43934411+denkaysik@users.noreply.github.com>
2020-12-17 18:47:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from configs.config import ApplicationConfig
+from transport.sanic.configure_sanic import configure_app
+
+
+def main():
+    config = ApplicationConfig()
+    app = configure_app()
+
+    app.run(
+        host=config.sanic.host,
+        port=config.sanic.port,
+        workers=config.sanic.workers,
+        debug=config.sanic.debug,
+    )
+
+
+if __name__ == '__main__':
+    main()
